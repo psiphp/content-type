f22c7869e70544f80af7e0dfe0aad2edb38a561b
dantleech <dan.t.leech@gmail.com>
2016-09-20 14:21:34 +0100

    Removed CMF headers

--- src/Metadata/Driver/AnnotationDriver.php
+++ src/Metadata/Driver/AnnotationDriver.php
@@ -1,21 +1,12 @@
 <?php
 
-/*
- * This file is part of the Symfony CMF package.
- *
- * (c) 2011-2016 Symfony CMF
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Psi\Component\ContentType\Metadata\Driver;
 
+use Doctrine\Common\Annotations\Reader;
 use Metadata\Driver\DriverInterface;
 use Psi\Component\ContentType\Metadata\Annotations;
 use Psi\Component\ContentType\Metadata\ClassMetadata;
 use Psi\Component\ContentType\Metadata\PropertyMetadata;
-use Doctrine\Common\Annotations\Reader;
 
 class AnnotationDriver implements DriverInterface
 {
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Symfony CMF package.
- *
- * (c) 2011-2016 Symfony CMF
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 $loader = require __DIR__ . '/../vendor/autoload.php';
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
